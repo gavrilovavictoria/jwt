ac9756b1e7d4d88fb05f342c59412a3cef5fb0a1
ArtemisZeep <aazemlyanukhin@gmail.com>
2023-11-18 12:56:14 +0300

    Fix

---
+++ api/middlewares/corsHandler.js
@@ -0,0 +1,33 @@
+const allowedCors = [
+    'https://localhost:3000',
+    'http://localhost:3000',
+    'https://localhost:3001',
+    'http://localhost:3001'
+  ];
+  
+  const corsHandler = (req, res, next) => {
+    const { origin } = req.headers;
+    const { method } = req; 
+  
+    // Значение для заголовка Access-Control-Allow-Methods по умолчанию (разрешены все типы запросов)
+    const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+    const requestHeaders = req.headers['access-control-request-headers'];
+    res.header('Access-Control-Allow-Credentials', true);
+    if (allowedCors.includes(origin)) {
+      // устанавливаем заголовок, который разрешает браузеру запросы с этого источника
+      res.header('Access-Control-Allow-Origin', origin);
+    }
+  
+    // Если это предварительный запрос, добавляем нужные заголовки
+    if (method === 'OPTIONS') {
+    // разрешаем кросс-доменные запросы любых типов (по умолчанию)
+      res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+      // разрешаем кросс-доменные запросы с этими заголовками
+      res.header('Access-Control-Allow-Headers', requestHeaders);
+      // завершаем обработку запроса и возвращаем результат клиенту
+      return res.end();
+    }
+    return next();
+  };
+  
+  module.exports = corsHandler;
